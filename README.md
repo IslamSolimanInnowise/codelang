75c09fc650f30c86d948c3c2c2cb645dcf85ffa7
IslamSolimanInnowise <islam.soliman@innowise.com>
2025-02-12 16:58:46 +0300

    refactor: extend login schema in register schema

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # codelang
 
-- schema inheritance for login (zod)
+- schema inheritance from login (zod) => done
+- code: login form thunk
+- replace useAppSelector and useAppDispatch by other custom hooks, for example:
+
+```ts
+const { getPosts, getPost, loading, posts, currentPost } = usePostsStore();
+```
--- src/widgets/ui/LoginForm/schema.ts
+++ src/widgets/ui/LoginForm/schema.ts
@@ -3,19 +3,21 @@ import { z } from "zod";
 const passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z0-9]).+$/;
 const minPasswordLength = 6;
 
+const passwordSchema = z
+  .string()
+  .regex(passwordPattern, {
+    message:
+      "Password must contain at least one lowercase letter, one uppercase letter, one number and one symbol!",
+  })
+  .min(minPasswordLength, {
+    message: "Password must be longer than or equal to 6 characters",
+  });
+
 const loginFormSchema = z.object({
   username: z.string().nonempty("Username required!").min(5, {
     message: "Username must be longer than or equal to 5 characters",
   }),
-  password: z
-    .string()
-    .regex(passwordPattern, {
-      message:
-        "Password must contain at least one lowercase letter, one uppercase letter, one number and one symbol!",
-    })
-    .min(minPasswordLength, {
-      message: "Password must be longer than or equal to 6 characters",
-    }),
+  password: passwordSchema,
 });
 
 type LoginSchema = z.infer<typeof loginFormSchema>;
@@ -25,4 +27,4 @@ const defaultValues: LoginSchema = {
   password: "",
 };
 
-export { defaultValues, type LoginSchema, loginFormSchema };
+export { defaultValues, type LoginSchema, loginFormSchema, passwordSchema };
